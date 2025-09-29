97d6ac921a5e5dfb434fcebf1a5fdc5ad1854f98
Cassio Rosa <cassioasrosa@gmail.com>
2025-09-29 16:39:35 -0300

    add menu

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 import funções as fnc
-#fnc.cadastrar_livros()
-fnc.atualizar_livro
-fnc.mostrar_livros()
-fnc.remover_livro
+import sqlite3
+
+
+while True:
+    print(f"""
+-----------------------------
+          SISTEMA 
+         BIBLIOTECA
+-----------------------------
+          
+    1 - Cadastrar livros
+          
+    2 - Mostrar os livros cadastrados
+          
+    3 - Alterar a disponibilidade de livros
+        
+    4 - Remover um livro cadastrado
+          
+    5 - Sair do sistema
+          
+-----------------------------
+
+    """)
+    escolha = int(input("Digite um numero para realizar a ação: "))
+    if escolha == 1:
+        fnc.cadastrar_livros()
+    elif escolha == 2:
+        fnc.mostrar_livros()
+    elif escolha == 3:
+        fnc.atualizar_livro()
+    elif escolha == 4:
+        fnc.remover_livro()
+    elif escolha == 5:
+        print("Ate a proxima")
+        break
+    else:
+        print("Insira um numero valido")
+
+
+
+
+
